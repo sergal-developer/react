fff1544225b12083af43cc4e190238a7d6cb44e4
Sergio Antonio Gallegos Alvarado <sergio.gallegos@globant.com>
2022-06-21 13:51:10 -0500

    Home Page

---
+++ website/src/routes/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Video from "../components/video/Video";
+import Footer from "../components/footer/Footer";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Video />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
