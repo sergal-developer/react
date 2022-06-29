9b1516b6e0d33237f90ddd8d54fdf6d0876b36b9
Sergio Antonio Gallegos Alvarado <sergio.gallegos@globant.com>
2022-06-29 11:02:43 -0500

    Base project

---
+++ master-react-example/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component1 } from './components/Component1';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+       
+      <Component1 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
