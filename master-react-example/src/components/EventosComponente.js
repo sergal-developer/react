5f0e5141d06bd23d68a6fec6f7465fa74cd0b160
Sergio Antonio Gallegos Alvarado <sergio.gallegos@globant.com>
2022-06-29 12:30:48 -0500

    Evento Doble Click

--- master-react-example/src/components/EventosComponente.js
+++ master-react-example/src/components/EventosComponente.js
@@ -4,9 +4,15 @@ export const EventosComponente = (props) => {
     const handlerClick = (e, nombre) => {
         alert(`${ nombre }, has dado click al boton`);
     }
+
+    const handlerDblClick = (e, nombre) => {
+        alert(`${ nombre }, has dado doble click al boton`);
+    }
   return (
     <div>
         <button onClick={ e => handlerClick(e, 'Sergio') }> Dame Click</button>
+
+        <button onDoubleClick={ e => handlerDblClick(e, 'Sergio') }> Doble Click</button>
     </div>
   )
 }
