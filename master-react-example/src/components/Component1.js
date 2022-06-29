6ddc7a5258cc3fe9923f6caa51d5d2512b85c3ec
Sergio Antonio Gallegos Alvarado <sergio.gallegos@globant.com>
2022-06-29 11:14:40 -0500

    Mostrar datos del compoente

--- master-react-example/src/components/Component1.js
+++ master-react-example/src/components/Component1.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 export const Component1 = () => {
+    let nombre = "sergio";
+    const web = "sergio.g.com";
+    let usuario = {
+        nombre: 'Sergio',
+        apellidos: 'Gallegos',
+        web: 'sergio.gal.com.mx'
+    }
+
   return (
     <>
-        <h1>Componente creado</h1>
-
+        <h1>Usuario</h1>
+        <ul>
+            <li>Nombre: <strong>{ usuario.nombre } { usuario.apellidos }</strong></li>
+            <li>Web: <strong>{ usuario.web }</strong></li>
+        </ul>
         <ul>
             <li>React</li>
             <li>Angular</li>
