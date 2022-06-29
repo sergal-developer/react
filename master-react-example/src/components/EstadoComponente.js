be38cc50105d7fc914feccec95b46aa863e351cb
Sergio Antonio Gallegos Alvarado <sergio.gallegos@globant.com>
2022-06-29 13:29:55 -0500

    User State con OnChange

--- master-react-example/src/components/EstadoComponente.js
+++ master-react-example/src/components/EstadoComponente.js
@@ -10,12 +10,17 @@ export const EstadoComponente = () => {
             setNombre('Sergio');
     }
 
+    const cambiarNombre = (e, nuevoNombre) => {
+            setNombre(nuevoNombre);
+    }
+
     return (
         <div>
             <h3>Manejo del estado</h3>
-            <strong>{nombre}</strong>
+            <strong className='label'>{nombre}</strong>
 
             <button onClick={cambiaNombre}> Cambiar Nombre </button>
+            <input type="text" onChange={ e => cambiarNombre(e, e.target.value ) }/>
         </div>
     )
 }
