286fc14abc8c251234d9f2e58d2f7c6d99ecde90
Sergio Antonio Gallegos Alvarado <sergio.gallegos@globant.com>
2022-07-07 02:58:07 -0500

    use navigate

--- master-routing/src/views/Persona.js
+++ master-routing/src/views/Persona.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const Persona = () => {
     let { nombre, apellido } = useParams();
+    const navigate = useNavigate();
+
     apellido = apellido || 'Gallegos';
 
-  return (
-    <div>
-        { nombre ? (
-            <>
-            <h1>Pagina Persona</h1>
-            <p>Esta persona { nombre } { apellido }</p>
-            </>
-        ) : ( <h1> No hay persona que mostrar </h1>)}
-    </div>
-  )
+    const onSubmit = e => {
+        e.preventDefault();
+        let _nombre = e.target.nombre.value;
+        let _apellido = e.target.apellido.value;
+
+        navigate(`/persona/${ _nombre }/${ _apellido }`);
+    }
+
+    return (
+        <div>
+            {nombre ? (
+                <>
+                    <h1>Pagina Persona</h1>
+                    <p>Esta persona {nombre} {apellido}</p>
+                </>
+            ) : (<h1> No hay persona que mostrar </h1>)
+            }
+
+            <form onSubmit={ onSubmit }>
+                <input type="text" name="nombre" />
+                <input type="text" name="apellido" />
+                <input type="submit" name="enviar" value="enviar" />
+            </form>
+        </div>
+    )
 }
